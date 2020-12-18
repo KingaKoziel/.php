72500889797de7d3d54ff66ac23fd1bfc2ec319a
KingaKoziel <74329530+KingaKoziel@users.noreply.github.com>
2020-12-18 08:45:08 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -35,9 +35,8 @@ echo('<table border="1"  width="100%" class="tabela"');
 	
 echo('<tr><th>Imie</th><th>zarobki</th><th>dzial</th></tr>');
 while($row=mysqli_fetch_assoc($result)){ 
-   
+   echo ('<tr>');
   echo('<td>'.$row['imie'].'</td><td>'.$row['zarobki'].'</td><td>'.$row['dzial'].'</td>');     
-  echo("<tr>"); 
   echo("</tr>"); } 
 echo('</table>'); 
   
