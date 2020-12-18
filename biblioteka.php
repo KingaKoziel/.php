38107d5e61e17837131c05569ea73f61d79b54a0
KingaKoziel <74329530+KingaKoziel@users.noreply.github.com>
2020-12-18 08:21:31 +0100

    Update biblioteka.php

--- biblioteka.php
+++ biblioteka.php
@@ -11,7 +11,7 @@
 <?php 
 require_once("connect.php");
   echo("<h3>Tytuły</h3>"); 
-$sql = "select * from biblTytul, biblAutor_biblTytul";
+$sql = "select * from biblTytul";
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);
 echo('<table border="1" class="tabela"'); 
