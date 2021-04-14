a18661668dd2486b224d70898ea8f7469a9c3261
KingaKoziel <koziel.kinga4@gmail.com>
2021-04-14 22:48:40 +0200

    a

--- Jednorozce/str1/strona.php
+++ Jednorozce/str1/strona.php
@@ -33,7 +33,7 @@
             echo("<th>ID</th><th>Auto</th>");
                 while($row = mysqli_fetch_assoc($result)) {
                     echo("<tr>");
-                    echo("<td>".$row['ID']."</td><td>".$row['Auto']."</td>");
+                    echo("<td>".$row['id']."</td><td>".$row['Auto']."</td>");
                     echo("</tr>");
                 };
             echo("</table>");
@@ -62,7 +62,7 @@
             echo("<th>ID</th><th>Kolor</th>");
                 while($row = mysqli_fetch_assoc($result)) {
                     echo("<tr>");
-                    echo("<td>".$row['ID']."</td><td>".$row['Kolor']."</td>");
+                    echo("<td>".$row['id']."</td><td>".$row['Kolor']."</td>");
                     echo("</tr>");
                 };
             echo("</table>");
@@ -71,7 +71,32 @@
         ?>
         </div>
 
-        <div class="p4">4</div>
+        <div class="p4">
+
+        <?php
+            require_once("../../connect.php");
+            $sql = "select Auta, `Kolor`, (`WDW`.ID) as ID_TAB from `kingakoziel_2`.WDW, `kingakoziel_2`.Auto, `kingakoziel_2`.Kolor where Kolor.id=Auta_id and Kolor.id=Kolor_id order by ID_TAB asc";
+                if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                 }
+                    $result = mysqli_query($conn, $sql);
+                if ( $result) {
+                } else {
+                    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                }
+            
+                echo("<h2>Wiele do Wielu</h2>");
+            
+                echo("<table border='1'>");
+                    while($row = mysqli_fetch_assoc($result)) {
+                        echo("<tr>");
+                        echo("<td>".$row['id_TAB']."</td><td>".$row['Auto']."</td><td>".$row['Kolor']."</td>");
+                        echo("</tr>");
+                    };
+                echo("</table>");
+                echo ("<br>");
+    ?>
+    </div>
         <div class="p5">5</div>      
     </div>
     </body>
