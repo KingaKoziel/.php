022d1251686f34f55f9684914c7b0de3519f1e7c
KingaKoziel <koziel.kinga4@gmail.com>
2021-04-15 10:13:35 +0200

    a

--- Jednorozce/str1/strona.php
+++ Jednorozce/str1/strona.php
@@ -73,30 +73,30 @@
 
         <div class="p4">
         <?php
-                        require_once("../connect.php");
-                        $sql = "select (`WID`.id) as ID_TAB, Kolor, Auto from Auta , Kolor where Auto.id=biblAuto_id and Kolor.id=Kolor_id order by ID_TAB asc";
-                            if ($conn->connect_error) {
-                                    die("Connection failed: " . $conn->connect_error);
-                             }
-                                $result = mysqli_query($conn, $sql);
-                            if ( $result) {
-                            } else {
-                                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-                            }
-                        
-                            echo("<h1>Wiele do Wielu</h1>");
-                        
-                            echo("<table border='1'>");
-                            echo("<th>ID</th><th>Auto</th><th>Autor</th>");
-                                while($row = mysqli_fetch_assoc($result)) {
-                                    echo("<tr>");
-                                    echo("<td>".$row['ID_TAB']."</td><td>".$row['Auto']."</td><td>".$row['Kolor']."</td>");
-                                    echo("</tr>");
-                                };
-                            echo("</table>");
-                            echo ("<br>");
-                ?>
-        </div>
+require_once ("../lib.php");
+
+$sql = "SELECT Auto_id, Auto , Kolor FROM Auto, Kolor WHERE Auto.ID = WID.id_Auto AND Kolor.ID = WID.id_Kolor";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>ID</th><th>Kolor</th><th>Auto</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['id_Auto'].'</td><td>'.$row['Auto'].'</td><td>'.$row['Kolor'].'</td>');
+        echo('</tr>');
+    }
+
+echo('</table>');
+
+?>
        
         <div class="p5">5</div>      
     </div>
